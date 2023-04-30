7aa88fc0c7dbd34f4c366343416ed79809eccc49
StefanT997Dev <stefan.tosic@fayebsg.com>
2023-04-30 20:50:12 +0200

    finish mobile first

--- src/pages/kontakt/index.tsx
+++ src/pages/kontakt/index.tsx
@@ -8,13 +8,7 @@ const ClientContactPage = () => {
 
     return (
         <div className={classes.client_contact}>
-            {!isMobile ? (
-                <div className={classes.client_contact__logo}>
-                    <Logo />
-                </div>
-            ) : null}
-            {isMobile ? <Logo /> : null}
-            <div className={classes.client_contact__content}>Pozovi da zakažeš konsultaciju na +381-60-6250-232 ili pošalji mail na stefantosic.dev@gmail.com</div>
+            <div className={classes.client_contact__content}>Pozovi da zakažeš konsultaciju na +381/60-6250-232 ili pošalji mail na stefantosic.dev@gmail.com</div>
             <br />
             <div className={classes.client_contact__back}>
                 <Link href="/">Natrag na početnu</Link>
