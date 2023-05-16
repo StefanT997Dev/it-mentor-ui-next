11d85a88f263c4d66048eaf4326d1069022fd468
StefanT997Dev <stefan.tosic@fayebsg.com>
2023-05-16 16:27:10 +0200

    add lazy loading to landing page youtube video

--- src/pages/programiranje-za-pocetnike/index.tsx
+++ src/pages/programiranje-za-pocetnike/index.tsx
@@ -55,6 +55,46 @@ const ClientLandingPage = () => {
                     <div className={classes.landing_client__header__video}>
                         <iframe
                             src="https://www.youtube.com/embed/b_jmIlVYcTo"
+                            srcDoc="<style>
+                                *{
+                                    padding:0;
+                                    margin:0;
+                                    overflow:hidden;
+                                }
+
+                                body, html{
+                                    height: 100%;
+                                }
+
+                                img, svg{
+                                    position: absolute;
+                                    width: 100%;
+                                    top: 0;
+                                    bottom: 0;
+                                    margin: auto;
+                                }
+
+                                svg{
+                                    filter: drop-shadow(1px 1px 10px hsl(206.5, 70.7%, 8%));
+                                    transition: all 250ms ease-in-out;
+                                }
+
+                                body:hover svg{
+                                    filter: drop-shadow(1px 1px 10px hsl(206.5, 0%, 10%));
+                                    transform: scale(1.2);
+                                }
+                            </style>
+                            <a href='https://www.youtube.com/embed/b_jmIlVYcTo?autoplay=1&mute=1'>
+                              <img src='https://img.youtube.com/vi/b_jmIlVYcTo/hqdefault.jpg'
+                              alt='kako naci posao u it industriji'>
+                              <svg xlmns='http://www.w3.org/2000/svg' width='64' height='64'
+                              viewBox='0 0 24 24' fill='none' stroke='#ffffff' stroke-width='2'
+                              stroke-linecap='round' stroke-linejoin='round' class='feather
+                              feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon
+                              points='10 8 16 12 10 16 10 8'></polygon></svg>
+                            </a>
+                            "
+                            loading="lazy"
                             title="YouTube video player"
                             className={classes.landing_client__header__video__responsive_iframe}
                             allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
