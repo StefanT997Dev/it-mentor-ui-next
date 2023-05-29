5b81c374d9dc30f4e26f55c59c53cd52b80dba2a
StefanT997Dev <stefan.tosic@fayebsg.com>
2023-05-29 06:18:55 +0200

    Implement GA4 through react-ga

---
+++ src/components/GoogleAnalytics/GoogleAnalytics.ts
@@ -0,0 +1,5 @@
+import ReactGA from 'react-ga';
+
+export const initGA4 = () => {
+    ReactGA.initialize('G-8QCZSJV271');
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import localFont from '@next/font/local';
+import { initGA4 } from '@/components/GoogleAnalytics/GoogleAnalytics';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 
 const myFont = localFont({
   src: "./Gilroy-ExtraBold.woff2",
@@ -8,6 +11,11 @@ const myFont = localFont({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initGA4();
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <main className={myFont.className}>
       <Component {...pageProps} />
