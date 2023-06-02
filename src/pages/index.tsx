13eaa1143f89d18dfd663d3e69b35a5e5cb2252e
StefanT997Dev <stefan.tosic@fayebsg.com>
2023-06-02 18:40:25 +0200

    test home page text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,6 +108,103 @@ export default function Home({ lqipMobile, lqipDesktop }: HomeProps) {
             </div>
           </div>
         </div>
+
+        <div className={classes.content__faq}>
+          <h2>IT mentorstvo:<br />Sve što trebaš znati.</h2>
+          <h3>Kako funkcioniše program obuke?</h3>
+          <div className={classes.faq__answer}>
+            Objasnićemo na primeru Stefana, mentora, i Miljana, polaznika.
+
+            Nakon inicijalnog propitivanja u cilju otkrivanja trenutnog znanja klijenta, Stefan je napravio personalizovani razvojni plan Miljanu i odmah su krenuli da rade na projektu.
+
+            Mentor je davao smernice šta i gde Miljan treba da istražuje kod kuće, radio proveru koda nakon svake završene funkcionalnosti aplikacije na kojoj su radili. Odgovarao na svako pitanje koje je Miljan imao.
+
+            Ispravljao greške, davao mu najbolje pristupe iz industrije tokom mentorstva, i s’ obzirom da je sada imao konekciju, Miljan se, posetom Pančevu, upoznao i sa radom jedne od IT firmi.
+          </div>
+          <h3>Ko su mentori i kakvo iskustvo imaju?</h3>
+          <div className={classes.faq__answer}>
+            Ono što nas izdvaja kod selekcije tutora jeste činjenica da mentor mora biti zapošljeni softverski inženjer.
+
+            Ovo je jedan od preduslova kojim osiguravamo da je u koraku sa vremenom.
+
+            Svi imaju višegodišnje iskustvo u IT industriji i prolaze kroz proces selekcije pre nego da postanu mentori.
+
+            Dakle, ko su oni? Tvoje buduće kolege.
+          </div>
+          <h3>Prijava za IT obuku</h3>
+          <div className={classes.faq__answer}>
+            Sve što trebaš uraditi jeste da pozoveš broj telefona na dnu ekrana.
+          </div>
+          <h3>Koliko traje obuka programera?</h3>
+          <div className={classes.faq__answer}>
+            Jedna od pogodnosti mentorstva jeste što je trajanje obuke fleksibilno u zavisnosti od tvojih obaveza.
+
+            Imamo obzir prema svima koji menjaju karijeru, roditelji su, studiraju i slično.
+
+            Ide se tempom koji tebi odgovara. Ako imaš da posvetiš puno vremena, odlično! Nećemo te sputavati i ubrzaćemo proces tvoje obuke.
+
+            Ukoliko imaš puno obaveza, takođe ćemo se pobrinuti da dođeš do istog cilja, prilagođeni tvom rasporedu.
+          </div>
+          <h3>Da li je potrebno prethodno iskustvo u programiranju da bih se prijavio/la za obuku?</h3>
+        </div>
+
+        <div className={classes.text}>
+          H2: Kako postati programer: Miljan Pjanović, bivši pica majstor, sada junior backend developer
+          H3: Otkrivanje strasti za programiranjem: Miljanova priča
+          H3: Prekvalifikacija za IT: Miljanovi izazovi i motivacija
+          H3: Da li je programiranje teško?
+          H3: Kursevi programiranja nasuprot mentorstvu
+          H3: Postizanje cilja: Kako je Miljan postao junior c sharp programer i ostvario svoje ambicije
+
+          H2: Kako naučiti programiranje: Jedinstveni pristup učenja kroz projekte
+          H3: Vodiće te it stručnjak i ubrzati tvoj napredak
+          Uz naš inovativni stil podučavanja kroz projekte, dobićeš priliku da brže postigneš svoj cilj prvog zaposlenja od ostalih.
+
+          Ovo će ti dati kompetitivnu prednost nad polaznicima kurseva i fakulteta jer ćeš jedini imati adekvatno znanje neophodno za prvi posao i projekte iza sebe koji na to ukazuju.
+
+          Nijedan poslodavac ne želi teoretičara sa parčetom papira koji će recitovati naučene definicije, već nekoga ko će moći da se nosi sa realnim poslovnim zahtevima.
+
+          Odmah po početku obuke, dobićeš priliku da se upustiš u izgradnju real-world aplikacija i programskih rešenja. Ovi projekti su osmišljeni da pređeš kroz njih sve od tehnologija koje će ti biti potrebne na prvom poslu.
+
+          Naši IT stručnjaci će te nadgledati tokom čitavog procesa, pružajući ti dragocene povratne informacije i usmeravanje. Oni će redovno pregledati tvoj kod, identifikovati potencijalne greške i pružiti smernice kako bi unapredio/la svoje veštine.
+
+          Ova direktna interakcija sa mentorima omogućiće ti da brzo stekneš uvid u najbolje prakse i razumeš efektivne načine rešavanja problema.
+
+          Uz mentorsku podršku, bićeš u mogućnosti da stekneš poverenje u svoje veštine i izgradiš portfolio impresivnih projekata.
+
+          Kada završiš naš program, imaćeš sve alate i veštine koje su ti potrebne za pronalaženje svog prvog posla kao programer.
+
+          H3: Soft skillovi: Razvijanje veština saradnje
+
+
+          Uz naš program, ne samo da ćeš steći tehničko znanje, već ćeš takođe imati priliku da razviješ važne veštine saradnje. Uvereni smo da su soft skillovi ključni za uspeh u IT industriji i zato posvećujemo posebnu pažnju njihovom razvijanju.
+
+          Naš fokus nije samo na kodiranju - mi te podstičemo da radiš u timskom okruženju, sarađuješ sa drugim polaznicima i mentorima, i stekneš veštine komunikacije, liderstva i efektivne saradnje. Kroz različite projekte, naučićeš kako uspešno komunicirati, razmenjivati ideje, rešavati probleme zajedno i doprineti timskom uspehu.
+
+          Poslodavci sve više traže programere koji ne samo da imaju tehničko znanje, već i sposobnost da efektivno sarađuju u timu. Uz našu pomoć, ti ćeš razviti te veštine i imati konkurentske prednosti na tržištu rada.
+
+          H3: Povećanje vidljivosti i zapošljivosti kroz developer portfolio
+
+          Jedan od ključnih koraka ka uspešnoj karijeri u razvoju softvera je imati impresivan developer portfolio koji prikazuje tvoje veštine, kreativnost i sposobnost da rešavaš stvarne probleme.
+
+          Tvoja kolekcija radova biće svedočanstvo dostignuća i veština koje si stekao kroz rad na projektima.
+
+          Imati konkretne primere softverskih rešenja u cv-u je snažan alat za privlačenje potencijalnih poslodavaca i povećanje tvojih šansi za dobijanje posla.
+
+          H3: Pravi scenariji iz IT industrije: Sticanje relevantnih veština za tržište rada
+
+          Ono što ćeš dobiti kod nas a ne možeš nigde drugde jeste simulirano poslovno okruženje.
+
+          Kao i na radnom mestu, imaćeš team lead-a sa kojim ćeš planirati task-ove, dobijati njegove smernice i povratne informacije pri završenju određenih funkcionalnosti aplikacije.
+
+          Koje tehnologije i programski jezici se uče kod vas?
+          Da li moram da imam diplomu kako bih ušao u IT industriju?
+          Privatnika uglavnom ne zanima da li imaš prosek 10.00,
+          njega jedino interesuje da li poseduješ veštine neophodne za obavljanje posla i možeš li, uz pomoć tih skill-ova, njemu doneti profit ili ne.
+
+          Diploma je neophodna da bi postao doktor ili pravnik? Da. Treba ti kako bi postao programer? Ne.
+        </div>
+
         <ClientContactButton />
       </div>
     </>
---
+++ src/pages/registracija/index.tsx
@@ -0,0 +1,20 @@
+import { MentorRegisterDto } from "@/models/mentorRegisterDto";
+import { useStore } from "@/stores/store";
+
+const RegisterPage = () =>{
+    const {
+        dataStore: { registerMentor }
+    } = useStore();
+
+    const tryRegisterMentor = async (values: MentorRegisterDto) => {
+        try {
+            await registerMentor(values)
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div></div>
+    );
+}
